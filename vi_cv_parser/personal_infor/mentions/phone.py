c27bdf57dd8eee312ab540c25fb8983cc3d2382b
Nguyễn Tử Toàn Lợi <33365328+toanloi2569@users.noreply.github.com>
2021-01-21 02:25:49 +0700

    Update phone.py

--- vi_cv_parser/personal_infor/mentions/phone.py
+++ vi_cv_parser/personal_infor/mentions/phone.py
@@ -9,6 +9,33 @@ from fonduer.candidates.mentions import MentionExtractorUDF
 from fonduer.parser.models import Document
     
 class MentionPhoneNumber(MentionSentences):
+#     def apply(self, doc):
+
+#         list_digit = ['0','1','2','3','4','5','6','7','8','9']
+#         symbol_plus = '+'
+#         sympol_bracket = "(+"
+
+#         start_rgx = r'(\d|\+|\(\+)'
+#         number_phone = '([(]?\+84[)]?[ ]*\d{1,3}|0\d{2,4})[ ]?(-|\.)?[ ]?\d{3,4}[ ]?(-|\.)?[ ]?\d{3,4}'
+#         for sentence in doc.sentences:
+#             text = sentence.text
+
+#             # Tìm vị trí bắt đầu của số điện thoại
+#             match = re.search(start_rgx, text)
+#             start = -1 if match is None else match.start()
+
+#             # Tìm vị trí kết thúc của số điện thoại
+#             end = -1
+#             for i in range(len(text)-1, 0, -1):
+#                 if text[i].isdigit() and re.search('[a-zA-Z]', text[start:i+1]) is None:
+#                     end = i   
+#                     break
+
+#             if start != -1 and start < end: 
+#                 yield TemporarySpanMention(
+#                     char_start=start, char_end=end+1, sentence=sentence
+#                 )   
+                
     def apply(self, doc):
 
         list_digit = ['0','1','2','3','4','5','6','7','8','9']
@@ -19,22 +46,21 @@ class MentionPhoneNumber(MentionSentences):
         number_phone = '([(]?\+84[)]?[ ]*\d{1,3}|0\d{2,4})[ ]?(-|\.)?[ ]?\d{3,4}[ ]?(-|\.)?[ ]?\d{3,4}'
         for sentence in doc.sentences:
             text = sentence.text
-
-            # Tìm vị trí bắt đầu của số điện thoại
-            match = re.search(start_rgx, text)
-            start = -1 if match is None else match.start()
-
-            # Tìm vị trí kết thúc của số điện thoại
-            end = -1
-            for i in range(len(text)-1, 0, -1):
-                if text[i].isdigit() and re.search('[a-zA-Z]', text[start:i+1]) is None:
-                    end = i   
-                    break
-
-            if start != -1 and start < end: 
-                yield TemporarySpanMention(
-                    char_start=start, char_end=end+1, sentence=sentence
-                )   
+            matches = re.finditer(start_rgx, text)
+            if matches is None:
+                start = -1
+            else:
+                for match in matches:
+                    start = match.start()
+                    end = -1
+                    for i in range(len(text)-1, 0, -1):
+                        if text[i].isdigit() and re.search('[a-zA-Z]', text[start:i+1]) is None:
+                            end = i   
+                            break
+                    if start != -1 and start < end: 
+                        yield TemporarySpanMention(
+                            char_start=start, char_end=end, sentence=sentence
+                        )
             
 def matcher_number_phone(mention):
     number_phone = mention.get_span()
